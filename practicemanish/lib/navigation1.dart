5bd206bcc3bb0b62ee2231449c42edf27ae8eaf5
Manish <maneesh.sap.ind@gmail.com>
2021-05-18 09:13:54 +0530

    navigationOption2

---
+++ practicemanish/lib/navigation1.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:practicemanish/login.dart';
+
+class NavigationOption1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("MyFirstPracticeApp"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: () {
+                debugPrint("Add Icon Click");
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add_alarm),
+              onPressed: () {
+                debugPrint("Add alarm Click");
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add_box),
+              onPressed: () {
+                debugPrint("Add Box Click");
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.backpack),
+              onPressed: () {
+                debugPrint("Add Navigation Click");
+              },
+            )
+          ],
+        ),
+        body: (ListView(
+          children: [
+            Text("Second Screen"),
+            RaisedButton(onPressed: () {
+              Navigator.pop(context);
+            })
+          ],
+        )));
+  }
+}
---
+++ practicemanish/lib/navigation2.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:practicemanish/login.dart';
+
+class NavigationOption2 extends StatelessWidget {
+  List<String> option = ["Add", "Delete", "Update", "Execute"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("MyFirstPracticeApp"),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (manish) {
+                return option
+                    .map((e) => PopupMenuItem(
+                          child: Text(e),
+                          value: e,
+                        ))
+                    .toList();
+              },
+              onSelected: (value) => debugPrint(value),
+            )
+          ],
+        ),
+        body: (ListView(
+          children: [
+            Text("Second Screen"),
+            RaisedButton(onPressed: () {
+              Navigator.pop(context);
+            })
+          ],
+        )));
+  }
+}
